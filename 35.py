4df9bbb2a62141f079e398676819820088c5c7d2
SouravPaul82 <67969504+SouravPaul82@users.noreply.github.com>
2022-08-13 13:34:37 +0530

    Create 35.py

---
+++ 35.py
@@ -0,0 +1,13 @@
+def searchInsert(self, nums: List[int], target: int) -> int:
+        l=0
+        u=len(nums)-1
+        while l<=u:
+            mid=(l+u)//2
+            if nums[mid]==target:
+                return mid
+            else:
+                if nums[mid]<target:
+                    l=mid+1
+                else:
+                    u=mid-1
+        return l
